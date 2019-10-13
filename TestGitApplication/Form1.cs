29ff705e6702fbe9bd988c1fba1a63563a542717
Bhawesh Gupta <talktobhawesh@gmail.com>
2019-10-13 23:31:23 +0530

    delegate Basics

--- TestGitApplication/Form1.cs
+++ TestGitApplication/Form1.cs
@@ -10,20 +10,49 @@ using System.Windows.Forms;
 
 namespace TestGitApplication
 {
+    public delegate string MyDelegate(string str);
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
         }
+        public void function(string str)
+        {
 
+        }
         private void button1_Click(object sender, EventArgs e)
         {
 
             //comment added
             //develop branch added
-            MessageBox.Show("hello");
+            //List<int> d = new List<int>();
+            //d.Add(1);
+            //d.Add(2);
+            //d.Add(3);
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            var dd = from number in numbers where (number%2)==0
+                     select number;
+            MyDelegate DelObj = new MyDelegate(myFun1);
+            
+            //DelObj += MyDelegate(myFun2);
+
+            MessageBox.Show(DelObj("ok1\t").ToString());
+            //DelObj -= (myFun1);
+            DelObj += (myFun2);
+            MessageBox.Show(DelObj("ok2\t").ToString());
 
         }
+        public string myFun1(string str)
+        {
+           return  str+"fun1";
+
+        }
+        public string myFun2(string str)
+        {
+            return str + "fun2";
+        }
+        
     }
 }
